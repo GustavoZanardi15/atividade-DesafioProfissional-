3e159641bb21db781ebbc0a5c83a2f0fafec0c44
Gustavo Ulian Zanardi <134564393+GustavoZanardi15@users.noreply.github.com>
2024-05-30 17:17:32 -0300

    Delete exceptions directory

--- exceptions/customException.js
+++
@@ -1,9 +0,0 @@
-class NotFoundException extends Error {
-    constructor(message) {
-      super(message);
-      this.name = "NotFoundException";
-    }
-  }
-  
-  module.exports = { NotFoundException };
-  
